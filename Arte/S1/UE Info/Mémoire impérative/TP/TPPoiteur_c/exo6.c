f3546c0ac65bb1d9f7179f9edaafb148a825ef89
ArteBulk <quentin.assuncao@gmail.com>
2024-11-07 14:17:23 +0100

    d

--- Arte/S1/UE Info/Mémoire impérative/TP/TPPoiteur_c/exo6.c
+++ Arte/S1/UE Info/Mémoire impérative/TP/TPPoiteur_c/exo6.c
@@ -13,9 +13,42 @@ void printDate(Date d) {
     printf("Date : %d/%d/%d\n", d.day, d.month, d.year);
 }
 
+void incrDate(Date * p) {
+    if (p->month == 1 || p->month == 3 || p->month == 5 || p->month == 7 || p->month == 8 || p->month == 10 || p->month == 12) {
+        if (p->month == 12 && p->day == 31) {
+            p->year++;
+            p->month = 1;
+            p->day = 1;
+        }
+        else if (p->day == 31) {
+            p->month++;
+            p->day = 1;
+        }
+        else {
+            p->day++;
+        }
+    }
+    else if (p->month == 2) {
+        if (p->day == 28) {
+            p->month++;
+            p->day = 1;
+        }
+    }
+    else {
+        if (p->day == 30) {
+            p->month++;
+            p->day = 1;
+        }
+    }
+}
+
 int main(void) {
 
-    Date td = {01,01,1970};
+    Date td = {31,12,1970};
+
+    Date * ptr = &td;
+
+    incrDate(ptr);
 
     printDate(td);
 
